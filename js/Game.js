78ef062375d5721be9f3469fb07e613452a7f7a6
Aaron Billings <asbillings2007@gmail.com>
2019-05-20 21:00:28 -0400

    created a Game and Phrase class and added instruction comments as outline

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,23 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {}
+
+/*
+ Create the Game class in the Game.js file.
+The class should include a constructor that initializes the following properties:
+missed: used to track the number of missed guesses by the player. The initial value is 0, since no guesses have been made at the start of the game.
+phrases: an array of five Phrase objects to use with the game. A phrase should only include letters and spaces— no numbers, punctuation or other special characters.
+activePhrase: This is the Phrase object that’s currently in play. The initial value is null. Within the startGame() method, this property will be set to the Phrase object returned from a call to the getRandomPhrase() method.
+The class should also have these methods:
+startGame(): hides the start screen overlay, calls the getRandomPhrase() method, and sets the activePhrase property with the chosen phrase. It also adds that phrase to the board by calling the addPhraseToDisplay() method on the active Phrase object.
+getRandomPhrase(): this method randomly retrieves one of the phrases stored in the phrases array and returns it.
+handleInteraction(): this method controls most of the game logic. It checks to see if the button clicked by the player matches a letter in the phrase, and then directs the game based on a correct or incorrect guess. This method should:
+Disable the selected letter’s onscreen keyboard button.
+If the phrase does not include the guessed letter, add the wrong CSS class to the selected letter's keyboard button and call the removeLife() method.
+If the phrase includes the guessed letter, add the chosen CSS class to the selected letter's keyboard button, call the showMatchedLetter() method on the phrase, and then call the checkForWin() method. If the player has won the game, also call the gameOver() method.
+removeLife(): this method removes a life from the scoreboard, by replacing one of the liveHeart.png images with a lostHeart.png image (found in the images folder) and increments the missed property. If the player has five missed guesses (i.e they're out of lives), then end the game by calling the gameOver() method.
+checkForWin(): this method checks to see if the player has revealed all of the letters in the active phrase.
+gameOver(): this method displays the original start screen overlay, and depending on the outcome of the game, updates the overlay h1 element with a friendly win or loss message, and replaces the overlay’s start CSS class with either the win or lose CSS class.
+ */
