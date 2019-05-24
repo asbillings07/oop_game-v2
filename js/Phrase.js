8d80755c813cff74d537d216f4df165c2eb36213
Aaron Billings <asbillings2007@gmail.com>
2019-05-24 10:38:24 -0400

    updated phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -44,12 +44,10 @@ class Phrase {
 
     $listLetters.each((index, letter) => {
       if (letter.textContent === letterMatch) {
-        $(letter).toggleClass("show letter");
+        $(letter)
+          .removeClass("hide letter")
+          .addClass("show");
       }
     });
-
-    // showMatchedLetter(): reveals the letter(s) on the board that matches the player's selection.
-    // To reveal the matching letter(s), select all of the letter DOM elements that have a CSS class name that matches the selected letter and
-    // replace each selected element's hide CSS class with the show CSS class.
   }
 }
